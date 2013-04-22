caf920c2792f20ca8e3cb2e2c801c33b3a0fca0e
Thomas Levine <occurrence@thomaslevine.com>
2013-04-22 19:57:30 -0400

    data

---
+++ q42.hs
@@ -0,0 +1,27 @@
+-- 42. What are you trying to achieve with your participation in the Occupy Movement.
+
+-- http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Haskell
+-- http://www.haskell.org/haskellwiki/Edit_distance
+
+dist :: Eq a => [a] -> [a] -> Int
+dist a b 
+    = last (if lab == 0 then mainDiag
+	    else if lab > 0 then lowers !! (lab - 1)
+		 else{- < 0 -}   uppers !! (-1 - lab))
+    where mainDiag = oneDiag a b (head uppers) (-1 : head lowers)
+	  uppers = eachDiag a b (mainDiag : uppers) -- upper diagonals
+	  lowers = eachDiag b a (mainDiag : lowers) -- lower diagonals
+	  eachDiag a [] diags = []
+	  eachDiag a (bch:bs) (lastDiag:diags) = oneDiag a bs nextDiag lastDiag : eachDiag a bs diags
+	      where nextDiag = head (tail diags)
+	  oneDiag a b diagAbove diagBelow = thisdiag
+	      where doDiag [] b nw n w = []
+		    doDiag a [] nw n w = []
+		    doDiag (ach:as) (bch:bs) nw n w = me : (doDiag as bs me (tail n) (tail w))
+			where me = if ach == bch then nw else 1 + min3 (head w) nw (head n)
+		    firstelt = 1 + head diagBelow
+		    thisdiag = firstelt : doDiag a b firstelt diagAbove (tail diagBelow)
+	  lab = length a - length b
+          min3 x y z = if x < y then x else min y z
+
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+http://bit.ly/openqueshighlightoccupygen
+http://www.occupyresearch.net/2012/03/23/preliminary-findings-occupy-research-demographic-and-political-participation-survey/
